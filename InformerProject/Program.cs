d77117ee1a81cecfc463109688aa7be66ae52260
Mateusz Hubicz <hubicz15@gmail.com>
2022-06-04 16:01:44 +0200

    test wprowadzania danych oraz modyfikacji

--- InformerProject/Program.cs
+++ InformerProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,41 @@ namespace InformerProject
 {
     internal class Program
     {
+        private static string ConfigurationFile = "Config.cfg";
         static void Main(string[] args)
         {
+            //GetFilesFromDirectory();
+            CreateConfig();
+            Console.ReadKey();
+        }
+
+        public static void GetFilesFromDirectory()
+        {
+            string PathFolder = Directory.GetCurrentDirectory();                                             //folder w którym znajdują się pliki
+            string[] GetFilesFromDir = Directory.GetFiles(PathFolder);                       //lista plików w folderze
+            for (int i = 0; i < GetFilesFromDir.Length; i++)
+            {
+                string FileName = Path.GetFileName(GetFilesFromDir[i]);
+                Console.WriteLine(FileName);
+                var LastModify = File.GetLastWriteTime(GetFilesFromDir[i]);                          //Pobranie danych o ostatniej modyfikacji pliku
+                Console.WriteLine(LastModify);
+            }
+
+        }
+        
+        
+        static void CreateConfig()
+        {
+            if (File.Exists(ConfigurationFile))
+            {
+                File.ReadAllLines(ConfigurationFile);
+            }
+            else
+            {
+                //File.Create(ConfigurationFile);
+                File.OpenWrite(ConfigurationFile);
+                File.ReadAllLines(ConfigurationFile);
+            }
         }
     }
 }
